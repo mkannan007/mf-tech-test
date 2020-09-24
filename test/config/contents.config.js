addf04a4286398502806731f022639e6d5d96d62
mkannan007 <37662555+mkannan007@users.noreply.github.com>
2020-09-24 20:12:02 +0100

    update contents

--- test/config/contents.config.js
+++ test/config/contents.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  homePageTitle: "The world’s leading software development platform · GitHub",
+  homePageTitle:
+    "'GitHub: The Largest and Most Advanced Open Source Development Platform - Secure Public & Private Repositories - Code Review - Codespaces - Actions - CI CD - Project Management - DevOps · GitHub",
   readmePageTitle:
-    "webdriverio/README.md at master · webdriverio/webdriverio · GitHub"
+    "webdriverio/README.md at master · webdriverio/webdriverio · GitHub",
 };
